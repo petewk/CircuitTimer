8710ca3470822e4749d40a1507f65cc856d4b2de
petewk <petewk1991@gmail.com>
2024-08-30 16:09:49 +0100

    Nav bar added
    Tab navigation working with symbols above names, next to work on filling out different pages

--- app/index.tsx
+++ app/index.tsx
@@ -3,22 +3,48 @@ import { Link } from "expo-router";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faList } from '@fortawesome/free-solid-svg-icons/faList';
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons/faStopwatch';
+import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
 
 // import screens
 import Circuits from "./Nav-Screens/Circuits";
 import Timer from "./Nav-Screens/Timer";
 import Settings from "./Nav-Screens/Settings";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 
-const homeStack = createNativeStackNavigator()
+const homeStack = createMaterialTopTabNavigator()
 
 export default function Index() {
   return (
       <NavigationContainer independent={true}>
-        <homeStack.Navigator initialRouteName="Timer">
-          <homeStack.Screen name="Circuits" component={Circuits}/>
-          <homeStack.Screen name="Timer" component={Timer}/>
-          <homeStack.Screen name="Settings" component={Settings}/>
+        <homeStack.Navigator 
+          initialRouteName="Timer"
+          >
+          <homeStack.Screen options={{
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faList} />
+            )
+          }} name="Circuits" component={Circuits}/>
+
+          <homeStack.Screen options={{
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faStopwatch} />
+            )
+          }}
+          name="Timer" component={Timer}/>
+          
+          <homeStack.Screen options={{
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faGear} />
+            )
+          }}
+          name="Settings" component={Settings}/>
         </homeStack.Navigator>
       </NavigationContainer>
   );
